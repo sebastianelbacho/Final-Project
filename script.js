bf483879dfdd887c25ebd9f3e142bdd1a5147d70
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2023-03-17 21:06:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,44 @@ document.body.classList.add('fade-out');
 window.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('fade-out');
 });
+
+
+document.body.classList.add('fade-out');
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.remove('fade-out');
+});
+
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+
+    }
+}
+
+
+
+
+
+
+
+function validateForm() {
+    let x = document.forms["myForm"]["sname"].value;
+    if (x == "") {
+        alert("Second Name must be filled out");
+        return false;
+    }
+}
+
+
+function validateForm() {
+    let x = document.forms["myForm"]["pnum"].value;
+    if (x == "") {
+        alert("Phone number must be filled out");
+        return false;
+    }
+}
